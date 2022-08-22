dc095d29927c88a3fbf89af092289c983cfe9c6c
Benitha99 <benithadusabemariya@gmail.com>
2022-08-22 12:15:32 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState,useEffect } from 'react'
+import AddTask from "./components/AddTask";
+function App() {
+const[showAddTask,setShowAddTask]=useState(false);
+ const[tasks,setTasks]=useState([]);
+
+const deleteTask=(id)=>{
+  setTasks(tasks.filter((task)=>task.id !==id))
+}
+
+const toggleReminder=(id) =>{
+ setTasks(tasks.map((task)=>task.id===id ? {...task,reminder: !task.reminder}:task))
+ 
+}
+const addTask=(task)=>{
+  const id =Math.floor(Math.random() *10000) +1
+  const newTask={id, ...task}
+  setTasks([...tasks,newTask])
+
+}
+  return (
+    <div className="container">
+    <Header onAdd={()=>setShowAddTask(!showAddTask)}
+    showAdd={showAddTask}/>
+    {showAddTask && <AddTask onAdd={addTask}/>}
+    { tasks.length>0 ?<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> :'NO TASK TO SHOW'}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Button } from './Button';
+
+const Header = (props) => {
+ 
+  return (
+    <header className='header' >
+      <h1>TASK TRACKER</h1>
+      <Button color={props.showAdd ? 'red' : 'green'} text={props.showAdd ? 'Close' : 'Add'} onClick={props.onAdd}/>
+      
+    </header>
+  )
+}
+
+export default Header
